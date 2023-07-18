9e94e087f7d67b8e57150caa148d2e7b848b6a1c
kocpiepait <f.barahama01@gmail.com>
2023-07-18 17:32:51 +0700

    movie-catalog

---
+++ src/scripts/global/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '89540c7808230fbe14d0d37a8287f0d7',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/responsive.css';
+import App from './views/app';
+
+console.log('Hello Coders!');
+
+// eslint-disable-next-line no-unused-vars
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
